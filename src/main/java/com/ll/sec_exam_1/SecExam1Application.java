6e50083cbab18b7f6dfa27ca50688bd394982070
jhs512 <jangka512@gmail.com>
2022-07-30 15:53:13 +0900

    세팅

---
+++ src/main/java/com/ll/sec_exam_1/SecExam1Application.java
@@ -0,0 +1,13 @@
+package com.ll.sec_exam_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecExam1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecExam1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/sec_exam_1/SecExam1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sec_exam_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecExam1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
